97c724b288c0b72372f35bc1199bd31700540239
bludnov <207432455+invokestring@users.noreply.github.com>
2025-09-17 14:56:45 +0600

    1

--- Config.py
+++ Config.py
@@ -1,6 +1,7 @@
 # Брать с https://my.telegram.org
-API_ID = 24266760
-API_HASH = 'e8ed629b02d5af8e2ab2c4705ec53b62'
+API_ID = 123
+API_HASH = ''
 
 # Это не трогайте.
+
 PATTERN = r'(https?://t\.me/send\?start=([a-zA-Z0-9_-]+))'
